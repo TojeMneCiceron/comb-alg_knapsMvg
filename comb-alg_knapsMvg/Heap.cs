25added00d67580e9e11056524f9a4f61962842a
TojeMneCiceron <skorobogatova.mm5910@gmail.com>
2022-08-10 22:41:15 +0500

    final

---
+++ comb-alg_knapsMvg/Heap.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace knapsMvg
+{
+    class HeapNode<T>
+    {
+        public HeapNode(double key, T value, int level, double cost, double mass, string mask)
+        {
+            Key = key;
+            Value = value;
+            Level = level;
+            Cost = cost;
+            Mass = mass;
+            Mask = mask;
+        }
+
+        public double Key { get; set; }
+        public T Value { get; set; }
+        public int Level { get; set; }
+        public double Cost { get; set; }
+        public double Mass { get; set; }
+        public string Mask { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{Key}({Value})";
+        //}
+    }
+    class Heap<T>
+    {
+        int n;
+        List<HeapNode<T>> nodes;
+
+        public Heap()
+        {
+            n = 0;
+            nodes = new List<HeapNode<T>>();
+            nodes.Add(null);
+        }
+        public static void Swap(ref HeapNode<T> a, ref HeapNode<T> b)
+        {
+            HeapNode<T> temp = a;
+            a = b;
+            b = temp;
+        }
+        public HeapNode<T> Min()
+        {
+            return nodes[1];
+        }
+        public bool Insert(double new_key, T new_value, int new_level, double new_cost, double new_mass, string new_mask)
+        {
+            n++;
+            nodes.Insert(n, new HeapNode<T>(new_key, new_value, new_level, new_cost, new_mass, new_mask));
+            //nodes[n] = new HeapNode<T>(new_key, new_value, new_level, new_cost, new_mass, new_mask);
+
+            for (int i = n; i > 1 && nodes[i].Key > nodes[i / 2].Key; i /= 2)
+            //Swap(ref nodes[i], ref nodes[i / 2]);
+            {
+                HeapNode<T> temp = nodes[i];
+                nodes[i] = nodes[i / 2];
+                nodes[i / 2] = temp;
+            }
+
+            return true;
+        }
+        public HeapNode<T> DeleteMax()
+        {
+            if (n == 0)
+                return null;
+
+            HeapNode<T> max = nodes[1];
+
+            nodes[1] = nodes[n];
+            //nodes.RemoveAt(n);
+            n--;
+
+            Heapify(1);
+
+            return max;
+        }
+        void Heapify(int i)
+        {
+            for (; ; )
+            {
+                int left = 2 * i;
+                int right = 2 * i + 1;
+
+                int max = i;
+
+                if (left <= n && nodes[left].Key >= nodes[right].Key && nodes[left].Key >= nodes[i].Key)
+                { 
+                    max = left; 
+                }
+
+                if (right <= n && nodes[right].Key >= nodes[left].Key && nodes[right].Key >= nodes[i].Key)
+                { 
+                    max = right;
+                }
+
+                if (max == i)
+                    break;
+
+                //Swap(ref nodes[i], ref nodes[max]);
+
+                HeapNode<T> temp = nodes[i];
+                nodes[i] = nodes[max];
+                nodes[max] = temp;
+
+                i = max;
+            }
+        }
+        public override string ToString()
+        {
+            //if (n == 0)
+            //    return "";
+
+            //string res = $"{nodes[1].Key}\n";
+
+            //int a = 4;
+
+            //for (int i = 2; i <= n; i++)
+            //{
+            //    res += $"{nodes[i].Key} ";
+
+            //    if (i + 1 == a)
+            //    {
+            //        res += "\n";
+            //        a *= 2;
+            //    }
+            //}
+
+            //return res;
+
+            var nodes1 = nodes.GetRange(0, n + 1);
+
+            return n == 0 ? "" : nodes1.Where(x => x != null).Select(x => x.Key.ToString("0.0")).Aggregate((x, y) => $"{x}\t{y}");
+        }
+    }
+}
---
+++ comb-alg_knapsMvg/mvg.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace knapsMvg
+{
+    class Item
+    {
+        int n;
+        double e, m, c;
+
+        public Item(int n, double m, double c)
+        {
+            this.n = n;
+            this.m = m;
+            this.c = c;
+            e = c / m;
+        }
+
+        public int N { get => n; set => n = value; }
+        public double M { get => m; set => m = value; }
+        public double C { get => c; set => c = value; }
+        public double E { get => e; set => e = value; }
+    }
+    class mvg
+    {
+        static void Read(ref int N, ref int M, ref List<Item> items)
+        {
+            StreamReader streamReader = new StreamReader(@"C:\Users\Пользователь\source\repos\knapsMvg\knapsMvg\input.txt");
+            var temp = streamReader.ReadLine().Split(' ');
+            N = int.Parse(temp[0]);
+            M = int.Parse(temp[1]);
+
+            //m
+            temp = streamReader.ReadLine().Split(' ');
+            //c
+            var temp2 = streamReader.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                double m = double.Parse(temp[i]);
+                double c = double.Parse(temp2[i]);
+                items.Add(new Item(i + 1, m, c));
+            }
+
+            streamReader.Close();
+        }
+
+        static void Print(List<Item> items)
+        {
+            items.Sort((x, y) => x.N.CompareTo(y.N));
+
+            string res = items.Count == 0 ? "мдауш" : items.Select(x => x.N.ToString()).Aggregate((x, y) => $"{x} {y}");
+
+            Console.WriteLine(res);
+        }
+
+        static double F(List<Item> S, int M, int i, double cost, double mass)
+        {
+            double e = i >= S.Count ? 0 : S[i].E;
+
+            return mass <= M ? (cost + (M - mass) * e) : -1;
+        }
+
+        static void MVG(List<Item> items, int M)
+        {
+            List<Item> res;
+            int n = items.Count;
+            Heap<List<Item>> heap = new Heap<List<Item>>();
+
+            while (true)
+            {
+                var curr = heap.DeleteMax();
+
+                if (curr is null)
+                    curr = new HeapNode<List<Item>>(0, new List<Item>(), 1, 0, 0, "");
+                //else
+                //    Console.WriteLine("Curr: " + curr.Key.ToString() + " Level: " + curr.Level.ToString() + " Mask: " + curr.Mask);
+
+                if (curr.Level == n + 1)
+                {
+                    res = curr.Value;
+                    break;
+                }
+
+                //Console.WriteLine(heap.ToString());
+
+                int i = curr.Level;
+
+                double S_plus = F(items, M, curr.Level, curr.Cost + items[i - 1].C, curr.Mass + items[i - 1].M);
+                double S_minus = F(items, M, curr.Level, curr.Cost, curr.Mass);
+
+                //Console.WriteLine(S_plus);
+                //Console.WriteLine(S_minus);
+                
+                var S = new List<Item>(curr.Value);
+                S.Add(items[i - 1]);
+
+                if (S_plus > -1)
+                    heap.Insert(S_plus, S, i + 1, curr.Cost + items[i - 1].C, curr.Mass + items[i - 1].M, curr.Mask + "+");
+                //Console.WriteLine(heap.ToString());
+                if (S_minus > -1)
+                    heap.Insert(S_minus, new List<Item>(curr.Value), i + 1, curr.Cost, curr.Mass, curr.Mask + "-");
+
+                //Console.WriteLine();
+                //Console.WriteLine(heap.ToString());
+                //Console.WriteLine();
+            }
+
+            Print(res);
+        }
+
+        static void Main(string[] args)
+        {
+            int N = 0, M = 0;
+            List<Item> items = new List<Item>();
+
+            Read(ref N, ref M, ref items);
+            items.Sort((x, y) => y.E.CompareTo(x.E));
+
+            MVG(items, M);
+        }
+    }
+}
